47ebed546cd82d4c1e5419507054a4dc39593006
Jeremy Chang <jeremy.chang@beonex.com>
2025-02-21 14:11:59 -0600

    Replace class with functions

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,97 @@
-import { L10n, type L10nConfig } from "./L10n";
+import { FormatValues, MessageFormatter, pluralTypeHandler } from "@ultraq/icu-message-formatter";
+import { generateMessageId } from "./generateMessageId";
 
-let l10n: L10n;
+const defaultHanlders = { plural: pluralTypeHandler };
+let messages: Record<string, string> = {};
+let msgCache = {};
+let formatter: MessageFormatter;
 
-export function createL10n(config: L10nConfig): L10n {
-  l10n = new L10n(config);
-  return new L10n(config);
+/**
+ * Loads messages and sets the locale
+ * @param locale 
+ * @param msgs 
+ */
+export function setLocale(locale: string, msgs: Record<string, string>) {
+  msgCache = {};
+  messages = msgs;
+  formatter = new MessageFormatter(locale, defaultHanlders);
 }
 
+export function plural(num: number, variations: Record<string, string>) {
+  let pluralOptions = "";
+  Object.entries(variations).forEach(([key, value]) => {
+    pluralOptions += ` ${key} {${value}}`;
+  });
+  let str = `{num, plural,${pluralOptions}}`;
+  let message: MessageDescriptor = {
+    id: generateMessageId(str),
+    defaultMessage: str,
+  };
+  return translateString(message, { num });
+}
+
+/**
+ * Interpolates and translates the given string
+ * ```
+ * $t`${num} apples`
+ * ```
+ * @param descriptor 
+ * @param values 
+ * @returns translated string
+ */
 export function t(descriptor: TemplateStringsArray, ...args: any[]): string {
-  if (!l10n) {
-    throw new Error('L10n instance not created');
+  let str: string = "";
+  // let segments = descriptor.findIndex((s) => this.commentsSeparator);
+  // if (segments.length > 2) {
+  //   str = segments[0];
+  // }
+  str = descriptor[0];
+  args.forEach((_arg, i) => {
+    str += `{${i}}` + descriptor[i + 1];
+  });
+  let msg: MessageDescriptor = {
+    id: generateMessageId(str),
+    defaultMessage: str,
   }
-  return l10n.t(descriptor,...args);
+  let values = { ...args };
+
+  return translateString(msg, values);
 }
 
-export function plural(num: number, variations: Record<string, string>): string {
-  if (!l10n) {
-    throw new Error('L10n instance not created');
+/**
+ * Translates based on a message descriptor and values
+ * @param descriptor
+ * @param values 
+ * @returns translated string
+ */
+export function translateString(descriptor: MessageDescriptor, values: FormatValues): string {
+  if (!formatter) {
+    throw new Error("Locale not set");
   }
-  return l10n.plural(num, variations);
+  let message: string = getTranslatedString(descriptor.id);
+  // @ts-ignore
+  if (!message) {
+    message = descriptor.defaultMessage;
+  }
+  return formatter.format(message, values);
+}
+
+function getTranslatedString(strID: string): string {
+  if (msgCache[strID]) {
+    return msgCache[strID];
+  }
+  let str = messages[strID];
+  msgCache[strID] = str;
+  return str;
+}
+
+export interface L10nConfig {
+  locale?: string;
+  messages?: object;
+}
+
+export interface MessageDescriptor {
+  id: string;
+  defaultMessage: string;
+  description?: string;
 }
---
+++ tests/lib.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest';
+import { setLocale, t, plural } from '../lib';
+
+test('Translation', () => {
+  let messages = {
+    'eiHe3D': 'Hola, {0}!',
+    "bxDnZ3": "{num, plural, one {día} other {días}}",
+  };
+  setLocale('es', messages);
+  let name = "Alice";
+  expect(t`Hello, ${name}!`).toBe('Hola, Alice!');
+  expect(t`${name}, Hello!`).that.toBe('Alice, Hello!');
+
+  expect(plural(1, { one: "day", other: "days" })).toBe("día");
+  expect(plural(2, { one: "day", other: "days" })).toBe("días");
+});
